764dabe77c21ad09edfc77ab8c4c646700e62ff1
nick <me@nickc.io>
2022-11-12 22:43:59 -0500

    feat: Configure mui library

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,31 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { EmotionCache, CacheProvider } from '@emotion/react';
+import { ThemeProvider, CssBaseline, createTheme } from '@mui/material';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import createEmotionCache from '../utils/createEmotionCache';
+import darkThemeOptions from '../styles/themes/darkThemeOptions';
+import '../styles/globals.css';
+
+interface ExtendedProps extends AppProps {
+  emotionCache?: EmotionCache;
 }
+
+const clientSideEmotionCache = createEmotionCache();
+const darkTheme = createTheme(darkThemeOptions);
+
+const App: React.FunctionComponent<ExtendedProps> = ({
+  Component,
+  pageProps,
+  emotionCache = clientSideEmotionCache,
+}: ExtendedProps) => {
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
+};
+
+export default App;
---
+++ pages/_document.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps,
+} from 'next/document';
+import createEmotionServer from '@emotion/server/create-instance';
+import createEmotionCache from '../utils/createEmotionCache';
+
+export default function ProfileDocument() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
+
+ProfileDocument.getInitialProps = async (
+  ctx: DocumentContext,
+): Promise<DocumentInitialProps> => {
+  const originalRenderPage = ctx.renderPage;
+  const cache = createEmotionCache();
+  const { extractCriticalToChunks } = createEmotionServer(cache);
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) =>
+        function EnhanceApp(props) {
+          /* @ts-ignore */
+          return <App emotionCache={cache} {...props} />;
+        },
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+  const emotionStyles = extractCriticalToChunks(initialProps.html);
+  const emotionStyleTags = emotionStyles.styles.map((style) => (
+    <style
+      data-emotion={`${style.key} ${style.ids.join(' ')}`}
+      key={style.key}
+      dangerouslySetInnerHTML={{ __html: style.css }}
+    />
+  ));
+
+  return {
+    ...initialProps,
+    styles: [
+      ...React.Children.toArray(initialProps.styles),
+      ...emotionStyleTags,
+    ],
+  };
+};
